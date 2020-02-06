7b896ef03dcda6989d0c7d4a7836622af38d6cf9
Nate C-K <nculwell@gmail.com>
2020-02-06 05:49:31 -0600

    Save source path of stats.

--- data/smogon/parse-moveset.py
+++ data/smogon/parse-moveset.py
@@ -12,20 +12,21 @@ def fail(msg, detail = None):
 def main():
     try:
         src_filename = sys.argv[1]
-        dst_filename = sys.argv[2]
     except IndexError:
         fail("invalid arguments", sys.argv[1:] or "(none)")
     if not os.path.exists(src_filename):
         fail("file not found", dst_filename)
+    noext, ext = os.path.splitext(src_filename)
+    dst_filename = noext + ".json"
     if dst_filename != "-" and os.path.exists(dst_filename):
         fail("file exists, will not overwrite", dst_filename)
     with open(src_filename, encoding='utf8') as src:
         stats = parse_stats(src)
     with open_dst(dst_filename) as dst:
-        write_stats(dst, stats)
+        write_stats(dst, stats, src_filename)
 
-def write_stats(dst, stats):
-    json.dump(stats, dst, indent=2)
+def write_stats(dst, stats, src_filename):
+    json.dump({ "source": src_filename, "stats": stats }, dst, indent=2)
 
 def open_dst(dst_filename):
     if dst_filename == "-":
