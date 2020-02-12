0678fca69ee14ea6a55e050268249f716060d3a0
Nate C-K <nculwell@gmail.com>
2020-02-12 06:23:49 -0600

    Generate moveset JSON.

--- data/gen-moveset-json.py
+++ data/gen-moveset-json.py
@@ -3,6 +3,8 @@
 import sys, os, os.path, json, re, subprocess
 import sqlite3
 
+DAMAGE_CLASS_LENGTH_CHARS = 4
+
 def fail(msg, detail = None):
     if detail:
         print("Error, " + msg + ": " + str(detail), file=sys.stderr)
@@ -48,8 +50,9 @@ def main():
     leads = read_leads_file(src_filename_leads)
     db = sqlite3.connect("pokeapi.db")
     db.text_factory = lambda x: str(x, DB_ENCODING)
+    moveset_data = get_moveset_data(dst, db, moveset, leads)
     with open_dst(dst_filename) as dst:
-        write_moveset(dst, db, moveset, leads)
+        json.dump(moveset_data, dst)
     db.close()
 
 def read_moveset_file(src_filename_moveset):
@@ -79,118 +82,85 @@ def open_dst(dst_filename):
     else:
         return open(dst_filename, mode='w')
 
-def write_moveset(dst, db, moveset, leads):
+def get_moveset_data(dst, db, moveset, leads):
     move_cache = {}
-    natures = load_natures(db)
-    original_source = moveset["source"]
-    source_filename = os.path.basename(original_source)
-    src_match = re.match(r"gen(\d+)([a-z]+)", source_filename)
+    moveset_source_filename = os.path.basename(moveset["source"])
+    src_match = re.match(r"gen(\d+)([a-z]+)", moveset_source_filename)
     src_generation, src_tier = src_match.group(1, 2)
     src_format = "Gen %s %s" % (src_generation, src_tier.upper())
-    type_eff = load_type_efficacy(db, src_generation)
-    moveset_stats = moveset["stats"]
+    type_eff_table = load_type_efficacy(db, src_generation)
     leads_index = build_leads_index(leads)
-    dst.write((r"{\footer \par {\qc\f2\fs%d {\b %s.} Based on Smogon.com usage statistics with additional data from PokeAPI. Source: %s \par\qr Page \chpgn}}" + NEWLINE)
-            % (int(FONT_SIZE_NORMAL * 2), src_format, original_source))
-    dst.write(BEGIN_COLUMNS + NEWLINE)
-    with open("moveset-stats-intro.rtf") as f:
-        dst.write(f.read().strip() + NEWLINE)
-    write_divider(dst)
-    pokemon_stats = []
-    for s in moveset_stats:
-        #print(s, file=sys.stderr)
+    compiled_pokemon_data = []
+    for s in moveset["stats"]:
         name = s["name"]
         identifier = pokemon_identifier_from_name(name)
         pokemon_info = lookup_pokemon_details(db, src_generation, identifier)
-        dst.write(r"\par\sa%d {\f0\fs%d\b\scaps %s [%s] #%s}" % (
-            POKEMON_ENTRY_PRE_SPACE, FONT_SIZE_HEADING * 2, name.upper(),
-            render_types(pokemon_info["types"]),
-            pokemon_info["national_pokdex_number"],
-            ))
-        dst.write(NEWLINE)
-        #dst.write(NEWLINE)
-        #write_types(dst, pokemon_info["types"])
-        write_stats(dst, natures, pokemon_info["stats"])
-        write_type_eff(dst, type_eff, pokemon_info["types"])
-        pairs_para(dst, "Abilities", s["abilities"])
-        pairs_para(dst, "Items", s["items"])
-        #pairs_para(dst, "EVs", s["spreads"])
-        #pairs_para(dst, "Moves", s["moves"])
-        write_moves(dst, db, move_cache, src_generation, s["moves"])
-        pairs_para(dst, "Teammates", s["teammates"])
+        type_efficacy = get_type_efficacy(type_eff_table, pokemon_info["types"])
+        upscaled_hp = int(1.25 * (int(2 * pokemon_info["stats"]["hp"] / 100) + 100 + 10))
+        moves = get_moves(db, move_cache, src_generation, s["moves"])
+        counters = get_counters(s["counters"])
+        lead_rank = leads_index.get(identifier)
+        compiled_pokemon_data.append({
+            "identifier": identifier,
+            "name": name,
+            "national_pokdex_number": pokemon_info["national_pokdex_number"],
+            "pokemon_db_details": pokemon_info,
+            "types": pokemon_info["types"],
+            "base_stats": pokemon_info["stats"],
+            "upscaled_hp": upscaled_hp,
+            "type_efficacy": type_efficacy,
+            "lead_rank": lead_rank,
+            "viability_ceiling": s["via_ceil"],
+            "raw_count": s["raw_count"],
+            "average_weight": s["avg_weight"] if avg_weight != "---" else None,
+            "abilities": s["abilities"],
+            "items": s["items"],
+            "spreads": s["spreads"],
+            "moves": moves,
+            "teammates": s["teammates"],
+            "counters": counters,
+            })
+    compiled_moveset_data = {
+        "source_filename": source_filename,
+        "generation": src_generation,
+        "tier": src_tier,
+        "leads": leads,
+        "pokemon": compiled_pokemon_data,
+        "moves_used": sorted([ [ m["identifier"], m["id"] ] for m in move_cache ]),
+        }
+    return compiled_moveset_data
+
+def get_counters(moveset_counters):
         if not s["counters"]:
-            counters = None
+            counters = []
         else:
+            c = moveset_counters
             counters = [
                     {
-                        usage: trunc(c["usePct1"]),
-                        name: c["name"],
-                        pctKO: c["pctKO"],
-                        pctSwitch: c["pctSwitch"],
+                        "name": c["name"],
+                        "pct_usage": float(c["usePct1"]),
+                        "pct_ko": float(c["pctKO"]),
+                        "pct_switch": float(c["pctSwitch"]),
                     }
                     for c in s["counters"]
                 ]
-        lead_rank = leads_index.get(identifier)
-        avg_weight = cond_fmt(s["avg_weight"], (lambda aw: "%0.3f" % float(aw)))
-        pokemon_stats.append({
-            lead_rank: lead_rank,
-            viability_ceiling: s["via_ceil"],
-            raw_count: s["raw_count"],
-            average_weight: avg_weight if avg_weight != "---" else None,
-            counters: counters,
-            })
-    dstjs = {
-            source_filename: source_filename,
-            generation: src_generation,
-            tier: src_tier,
-            leads: leads,
-            moves: move_cache,
-            }
+        return counters
 
-def write_move_reference(dst, move_cache):
-    dst.write(r"\par\sb%d {\s0 {\f0\fs%d\b MOVE REFERENCE}}" % (
-        MOVE_REFERENCE_PRE_SPACE, FONT_SIZE_HEADING * 2))
-    dst.write(NEWLINE)
-    for identifier in sorted(move_cache.keys()):
-        dst.write(render_move(None, move_cache[identifier], True))
-        dst.write(NEWLINE)
-    dst.write(r"}")
-    dst.write(NEWLINE)
-
-def write_type_eff(dst, type_eff, types):
+def get_type_efficacy(type_eff_table, types):
     weak_to = []
     resists = []
-    for damage_type in type_eff["TYPES"]:
+    for damage_type in type_eff_table["TYPES"]:
         eff = 1.0
         for target_type in types:
-            eff = eff * type_eff[damage_type][target_type]
+            eff = eff * type_eff_table[damage_type][target_type]
         if eff < 1.0:
             resists.append([damage_type, eff])
         elif eff > 1.0:
             weak_to.append([damage_type, eff])
-    def write_eff_section(title, type_eff_list):
-        dst.write(r"{{\b %s:}\scaps " % title)
-        if len(type_eff_list) == 0:
-            dst.write("(none) ")
-        else:
-            dst.write(",".join(
-                [ " %s %s" % (t.title(), render_factor(e))
-                    for (t, e) in type_eff_list ]))
-        dst.write("}" + NEWLINE)
-    dst.write(r"{\line" + NEWLINE)
-    write_eff_section("Resists", resists)
-    write_eff_section("  Weak to", weak_to)
-    dst.write(r"}" + NEWLINE)
-
-def render_factor(fact):
-    if fact >= 1 or fact == 0:
-        return str(int(fact))
-    else:
-        mapping = { 0.25: CHAR_ONE_FOURTH, 0.50: CHAR_ONE_HALF, }
-        return mapping[fact]
-
-def write_divider(dst):
-    dst.write(r"\par" + (r"\emdash" * DIVIDER_EMDASH_COUNT) + NEWLINE)
+    return {
+            "resists": resists,
+            "weak_to": weak_to
+            }
 
 def build_leads_index(leads):
     ix = {}
@@ -198,40 +168,6 @@ def build_leads_index(leads):
         identifier = pokemon_identifier_from_name(x["pokemon"])
         ix[identifier] = x["rank"]
     return ix
-
-def cond_fmt(value, fmt_func):
-    try:
-        return fmt_func(value)
-    except:
-        return value
-
-def render_types(types):
-    #if types == [ "fairy" ]:
-    #    types = [ "normal" ];
-    return " ".join(( t.title() for t in types ))
-
-def write_types(dst, types):
-    #dst.write(r"\par {\s0 {\b %s:} \b " % "Type")
-    dst.write(r"{\line\s0\b")
-    for t in types:
-        dst.write(" " + t.upper())
-    dst.write(r"}")
-    dst.write(NEWLINE)
-
-def write_stats(dst, natures, stats):
-    #print("Stats: ", stats, file=sys.stderr)
-    eff = { s: calc_effective_stat(natures, s, stats[s], 100, "hardy", 0, 0)
-            for s in STAT_ORDER }
-    dst.write(r"{\line\s0")
-    #dst.write(r"{\b Stats:} ")
-    for s in STAT_ORDER:
-        if not s in stats:
-            raise Exception("Stat not found: " + s)
-    upscaled_hp = 5 * eff["hp"] / 4
-    dst.write(", ".join([ "%s %s" % (STAT_ABBRS[s], stats[s]) for s in STAT_ORDER ]))
-    dst.write(", Upscaled HP %d" % upscaled_hp)
-    dst.write(r"}")
-    dst.write(NEWLINE)
 
 STAT_ORDER = [
     "hp", "attack", "defense", "special-attack", "special-defense", "speed" ]
@@ -257,45 +193,17 @@ POKEMON_IDENTIFIER_ALTERNATES = {
         "shaymin": "shaymin-sky"
         }
 
-def write_moves(dst, db, move_cache, generation, moves):
-    dst.write(r"\par {\s0 {\b %s} " % "Moves")
-    move_text = []
+def get_moves(db, move_cache, generation, moves):
+    ms = []
     for m in moves:
-        if m[0] in [ "Other", "Nothing" ]:
-            move_text.append(r"\line %s %s" % (fmt_pct(m[1]), m[0]))
-            continue
-        md = lookup_move_details(db, move_cache, generation, m[0])
-        text = render_move(m[1], md, False)
-        move_text.append(text)
-    for mt in move_text:
-        dst.write(NEWLINE)
-        dst.write(mt)
-    dst.write(r"}")
-    dst.write(NEWLINE)
-
-def render_move(pct, md, is_reference):
-    damage_class = md["damage_class"][0:DAMAGE_CLASS_LENGTH_CHARS]
-    text = r"\line "
-    if pct:
-        text = text + fmt_pct(pct) + " "
-    if is_reference:
-        name_style = r"\b "
-    else:
-        name_style = ""
-    move_pp = int(md["pp"])
-    move_pp_max = int(move_pp * 1.6)
-    text = text + ("{%s%s:} {\scaps %s/%s}, PP %d-%d"
-            % (name_style, md["name"],
-                md["type"].title(), damage_class.title(),
-                move_pp, move_pp_max))
-    if md["power"]:
-        text = text + (r", Pow %s" % md["power"])
-    text = text + (r", Acc %s" % (md["accuracy"] if md["accuracy"] else "N/A"))
-    if md["priority"] and md["priority"] != "0":
-        text = text + (", Prio %+d" % int(md["priority"]))
-    if is_reference and md["short_effect"]:
-        text = text + (". {\i %s}" % md["short_effect"])
-    return text
+        move_name, usage_pct = m
+        if move_name in [ "Other", "Nothing" ]:
+            move_data = { "name": move_name }
+        else:
+            move_data = lookup_move_details(db, move_cache, generation, move_name)
+        move_data["usage_pct"] = m[1]
+        ms.append(move_data)
+    return ms
 
 def lookup_pokemon_details(db, generation, identifier):
     cur = db.cursor()
@@ -379,11 +287,13 @@ def lookup_move_details(db, move_cache, generation, move_name):
             "name": move_name,
             "identifier": row[1],
             "damage_class": row[2],
+            "damage_class_abbr": row[2][0:DAMAGE_CLASS_LENGTH_CHARS]
             "type": hidden_power_type or row[3],
-            "power": row[4] if not hidden_power_type else 70,
-            "pp": row[5],
-            "accuracy": row[6],
-            "priority": row[7],
+            "power": int(row[4]) if not hidden_power_type else 70,
+            "pp": int(row[5]),
+            "pp_max": int(float(row[5]) * 1.6),
+            "accuracy": int(row[6]),
+            "priority": int(row[7]),
             "effect_chance": row[8],
             "short_effect_template": row[9],
             "short_effect": format_effect(row[9], row[8])
@@ -405,66 +315,6 @@ def format_effect(effect_template, effect_chance):
     t = re.sub(r"\[\]\{[^:]:([^}]*)}", r"\1", t)
     return t
 
-def pairs_para(dst, section_title, pair_list):
-    dst.write(r"\par {\s0 {\b %s:} " % section_title)
-    dst.write(pairs(pair_list))
-    dst.write(r"}")
-    dst.write(NEWLINE)
-
-def counters_para(dst, counters):
-    dst.write(r"\par {\s0 {\b Counters} ")
-    dst.write(" ".join(
-        ( r"\line %d %s: KO %s, Switch %s" % (trunc(c["usePct1"]), c["name"], c["pctKO"], c["pctSwitch"])
-            for c in counters )))
-    dst.write(r"}")
-    dst.write(NEWLINE)
-    dst.write(r"\para")
-    dst.write(NEWLINE)
-
-def trunc(n):
-    return int(float(n))
-
-def pairs(pair_list):
-    return ", ".join(( "%s %s" % (fmt_pct(pair[1]), pair[0]) for pair in pair_list ))
-
-def fmt_pct(n):
-    x = str(int(float(n.strip("+%")))) + "%"
-    if n[0] == "+":
-        x = "+" + x
-    return x
-
-def calc_effective_stat(natures, stat_identifier, base, level, nature, iv, ev):
-    base, level, iv = int(base), int(level), int(iv)
-    if not ev is None:
-        ev = int(ev)
-    if stat_identifier == "hp":
-        return int((2 * base + iv + int(ev/4)) / 100) + level + 10
-    else:
-        nature_modifier = get_nature_modifier(natures, stat_identifier, nature)
-        return int((int(((2 * base + iv + int(ev/4)) * level) / 100) + 4) * nature_modifier)
-
-def get_nature_modifier(natures, stat_identifier, nature):
-    n = natures[nature]
-    if n[0] == stat_identifier:
-        return 0.9
-    elif n[1] == stat_identifier:
-        return 1.1
-    else:
-        return 1
-
-def load_natures(db):
-    cur = db.cursor()
-    natures_sql = """
-    select n.identifier, dec_s.identifier, inc_s.identifier
-    from natures n
-    join stats dec_s on dec_s.id = n.decreased_stat_id
-    join stats inc_s on inc_s.id = n.increased_stat_id
-    """
-    natures = {}
-    for row in cur.execute(natures_sql):
-        natures[row[0]] = (row[1], row[2])
-    return natures
-
 def load_type_efficacy(db, generation):
     generation = int(generation)
     cur = db.cursor()
@@ -478,8 +328,8 @@ def load_type_efficacy(db, generation):
     join types tt on tt.id = te.target_type_id
     """
     types = []
-    type_eff = {}
-    type_eff["TYPES"] = types
+    type_eff_table = {}
+    type_eff_table["TYPES"] = types
     type_generation = [ [ 2, "dark" ], [ 2, "steel" ], [ 6, "fairy" ] ]
     for row in cur.execute(type_eff_sql):
         dt_nm = row[1]
@@ -498,14 +348,14 @@ def load_type_efficacy(db, generation):
             while len(types) < dt_id:
                 types.append(None)
             types[dt_id-1] = dt_nm
-            te = type_eff.get(dt_nm)
+            te = type_eff_table.get(dt_nm)
             if not te:
                 te = {}
-                type_eff[dt_nm] = te
+                type_eff_table[dt_nm] = te
             te[tt_nm] = damage_factor_pct / 100.0
         except Exception as e:
             raise Exception("Error, dam=%s, tgt=%s: %s" % (dt_nm, tt_nm, str(e)))
-    return type_eff
+    return type_eff_table
 
 if __name__ == "__main__":
     main()
